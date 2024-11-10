a56f62249a6a691c248754a1306fe3fb75f7c175
TomislavRogic <tomislav.rogic@gmail.com>
2024-11-10 22:57:55 +0100

    TerminiPregled
    sada pri pregledu prikazuje Marku vozila i ime i prezime osobe- mislim da sve radi, ne vidim gresku

--- Frontend/src/pages/termini/TerminiPregled.jsx
+++ Frontend/src/pages/termini/TerminiPregled.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import skicatermina from '../../assets/skicatermina.png';
 import { APP_URL } from "../../constants";
 import Service from "../../services/TerminiService"; // Prilagođen import servisa
+import VozilaService from "../../services/VozilaService";
 import OsobaService from "../../services/OsobaService";
 import { RouteNames } from "../../constants";
 import moment from "moment-timezone";
@@ -15,10 +16,31 @@ function formatDate(dateString) {
 }
 
 export default function TerminiPregled() {
+    const [vozila, setVozila] = useState([]);
+    const [osobe, setOsobe] = useState([]);
     const [termini, setTermini] = useState([]);
     const [stranica, setStranica] = useState(1);
     const [uvjet, setUvjet] = useState('');
-    
+
+    async function dohvatiVozila() {
+        const odgovor = await VozilaService.get();
+        if (odgovor.greska) {
+            alert(odgovor.poruka);
+            return;
+        }
+        console.log('Vozila:', odgovor.poruka); // Dodano za provjeru podataka
+        setVozila(odgovor.poruka);
+    }
+
+    async function dohvatiOsobe() {
+        const odgovor = await OsobaService.get();
+        if (odgovor.greska) {
+            alert(odgovor.poruka);
+            return;
+        }
+        console.log('Osobe:', odgovor.poruka); // Dodano za provjeru podataka
+        setOsobe(odgovor.poruka);
+    }
 
     async function dohvatiTermine() {
         const odgovor = await Service.getStranicenje(stranica, uvjet);
@@ -30,25 +52,13 @@ export default function TerminiPregled() {
             setStranica(stranica - 1);
             return;
         }
+        console.log('Termini:', odgovor.poruka); // Dodano za provjeru podataka
         setTermini(odgovor.poruka);
     }
 
-    async function dohvatiOsobe() {
-        await OsobaService.get()
-            .then((odgovor) => {
-                if (!odgovor.greska) {
-                    // Ovdje možete postaviti podatke o osobama
-                    console.log(odgovor.poruka);
-                }
-            })
-            .catch((e) => {
-                console.error("Problem kod dohvaćanja osoba:", e);
-            });
-    }
     async function obrisiTermin(sifratermina) {
         try {
             const odgovor = await Service.obrisi(sifratermina);
-            // console.log(odgovor);
             if (odgovor.greska) {
                 alert(odgovor.poruka);
                 return;
@@ -60,22 +70,22 @@ export default function TerminiPregled() {
             alert("Došlo je do greške prilikom brisanja termina.");
         }
     }
-      
 
     useEffect(() => {
+        dohvatiVozila();
+        dohvatiOsobe();
         dohvatiTermine();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [stranica, uvjet]);
 
     function slika(termin) {
-        if(termin.slika != null){
+        if (termin.slika != null) {
             return APP_URL + termin.slika + `?${Date.now()}`;
         }
         return skicatermina;
     }
 
     function promjeniUvjet(e) {
-        if (e.nativeEvent.key == "Enter") {
+        if (e.nativeEvent.key === "Enter") {
             console.log('Enter');
             setStranica(1);
             setUvjet(e.nativeEvent.srcElement.value);
@@ -88,78 +98,69 @@ export default function TerminiPregled() {
     }
 
     function smanjiStranicu() {
-        if (stranica == 1) {
+        if (stranica === 1) {
             return;
         }
         setStranica(stranica - 1);
     }
 
     return (
-        
         <Container>
             <Row>
-            <Col key={1} sm={12} lg={4} md={4}>
+                <Col key={1} sm={12} lg={4} md={4}>
                     <Form.Control
-                    type='text'
-                    name='trazilica'
-                    placeholder='Dio imena i prezimena ili dio marke vozila [Enter]'
-                    maxLength={255}
-                    defaultValue=''
-                    onKeyUp={promjeniUvjet}
+                        type='text'
+                        name='trazilica'
+                        placeholder='Dio imena i prezimena ili dio marke vozila [Enter]'
+                        maxLength={255}
+                        defaultValue=''
+                        onKeyUp={promjeniUvjet}
                     />
                 </Col>
                 <Col key={2} sm={12} lg={4} md={4}>
-                {termini && termini.length > 0 && (
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Pagination size="lg">
-                    <Pagination.Prev onClick={smanjiStranicu} />
-                    <Pagination.Item disabled>{stranica}</Pagination.Item>
-                    <Pagination.Next
-                        onClick={povecajStranicu}
-                    />
-                </Pagination>
-            </div>
-                )}
+                    {termini && termini.length > 0 && (
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <Pagination size="lg">
+                                <Pagination.Prev onClick={smanjiStranicu} />
+                                <Pagination.Item disabled>{stranica}</Pagination.Item>
+                                <Pagination.Next onClick={povecajStranicu} />
+                            </Pagination>
+                        </div>
+                    )}
                 </Col>
                 <Col key={3} sm={12} lg={4} md={4}>
-            <Link to={RouteNames.TERMINI_DODAJ} className="btn btn-success gumb"> 
-                <IoIosAdd 
-                size={25} 
-                /> Dodaj
-            </Link>
-            </Col>
-             
+                    <Link to={RouteNames.TERMINI_DODAJ} className="btn btn-success gumb">
+                        <IoIosAdd size={25} /> Dodaj
+                    </Link>
+                </Col>
             </Row>
 
             <Row>
-
                 {termini && termini.map((termin) => (
-                   <Col key={termin.sifratermina} sm={12} lg={3} md={3}>
-                    <Card style={{ marginTop: '1rem' }}>
-                        <Card.Img variant="top" src={slika(termin)} className="slika"/>
-                        <Card.Body>
-                            <Card.Title>{termin.sifratermina}</Card.Title>
-                            <Card.Text>
-                                {termin.sifraosoba}
-                            </Card.Text>
-                            <Card.Text>
-                                {termin.sifravozila}
-                            </Card.Text>
-                            <Card.Text>
-                                {formatDate(termin.vrijemetermina)}
-                            </Card.Text>
-                            
-                            <Row>
-                                <Col>
-                                    <Link className="btn btn-primary gumb" to={`/termini/${termin.sifratermina}`}><FaEdit /></Link>
-                                </Col>
-                                <Col>
-                                <Button variant="danger" className="gumb"  onClick={() => obrisiTermin(termin.sifratermina)}><FaTrash /></Button>
-                                </Col>
-            </Row>
-        </Card.Body>
-    </Card>
-</Col>
+                    <Col key={termin.sifratermina} sm={12} lg={3} md={3}>
+                        <Card style={{ marginTop: '1rem' }}>
+                            <Card.Img variant="top" src={slika(termin)} className="slika" />
+                            <Card.Body>
+                                <Card.Title>{termin.vozilaMarka ? termin.vozilaMarka : 'Nepoznato vozilo'}</Card.Title>
+                                <Card.Text>{termin.osobaIme ? termin.osobaIme : 'Nepoznata osoba'}</Card.Text>
+                                <Card.Text>
+                                    {termin.sifravozila}
+                                </Card.Text>
+                                <Card.Text>
+                                    {formatDate(termin.vrijemetermina)}
+                                </Card.Text>
+
+                                <Row>
+                                    <Col>
+                                        <Link className="btn btn-primary gumb" to={`/termini/${termin.sifratermina}`}><FaEdit /></Link>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="danger" className="gumb" onClick={() => obrisiTermin(termin.sifratermina)}><FaTrash /></Button>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 ))}
             </Row>
             <hr />
